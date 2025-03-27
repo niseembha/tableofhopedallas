8122c54f58521eaa47f5e3e7f21f813b59d9a25f
Niseem <niseem2008@gmail.com>
2025-03-26 21:49:55 -0500

    added meals served text

--- app/page.js
+++ app/page.js
@@ -4,8 +4,6 @@ import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 import Head from 'next/head';
 
-
-
 export default function Home() {
 
   return (
@@ -19,6 +17,10 @@ export default function Home() {
         <h1>Welcome to Table of Hope Dallas</h1>
         <p>
           We're here to make a difference in our community.
+        </p>
+        {/* Meals Served Counter */}
+        <p className="meals-served">
+        Meals Served So Far: <span id="meals-count">25+</span>
         </p>
         <a 
           href="https://www.zeffy.com/en-US/fundraising/support-fresh-meals-for-the-homeless?fbclid=PAZXh0bgNhZW0CMTEAAabfSUh6MM6c2ovxab_l8f-nOHoG-FQkObxfwfIF06uvCDbpzxItoMZagjs_aem_ON2DJ8V0kWVKsizGFbs54A" 
